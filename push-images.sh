863aecc6b233fd401d0a88fdc41f5b5650935703
xtrntr <kangren.chia@gmail.com>
2020-11-04 11:51:44 +0800

    chore: make build specific for pipeline workers

--- push-images.sh
+++ push-images.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "${DOCKER_PASSWORD}" | docker login -u="${DOCKER_USERNAME}" --password-stdin
 
-IMAGE_NAME=${IMAGE_NAME:-spark-k8s-addons}
+IMAGE_NAME=${IMAGE_NAME:-spark-k8s-addons-ds}
 
 TAG_NAME="${SELF_VERSION}_${SPARK_VERSION}_hadoop-${HADOOP_VERSION}_scala-${SCALA_VERSION}_python-${PYTHON_VERSION}"
 docker tag "${IMAGE_NAME}:${TAG_NAME}" "${IMAGE_ORG}/${IMAGE_NAME}:${TAG_NAME}"
